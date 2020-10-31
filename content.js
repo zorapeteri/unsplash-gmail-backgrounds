461a579f6b188d68b2ed3d278dcb098a74e2dbb2
Zora Peteri <zorapeteri@gmail.com>
2020-10-31 09:06:04 +0100

    Initial commit

---
+++ content.js
@@ -0,0 +1,10 @@
+
+const style = document.createElement("style");
+style.innerHTML =
+`
+  .a4t {
+    background-image: url(https://source.unsplash.com/random/1920x1080) !important;
+  }
+`;
+
+document.body.appendChild(style);
