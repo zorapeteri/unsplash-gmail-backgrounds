316206c8af1fc4eed3f838027ddb80ae39e12e4d
Zora Peteri <zorapeteri@gmail.com>
2021-01-13 08:19:40 +0100

    linting

--- content.js
+++ content.js
@@ -1,7 +1,5 @@
-
-const style = document.createElement("style");
-style.innerHTML =
-`
+const style = document.createElement('style');
+style.innerHTML = `
   .a4t {
     background-image: url(https://source.unsplash.com/random/1920x1080) !important;
   }
